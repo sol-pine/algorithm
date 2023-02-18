a18dd24f4d5f2217568a2706dc1bdd030368ffa2
조해솔 <105091138+sol-pine@users.noreply.github.com>
2023-02-19 01:31:21 +0900

    Update and rename lv 0/각도기_120829.js to programmers/level 0/각도기.md

--- lv 0/각도기_120829.js
+++
@@ -1,7 +0,0 @@
-// 2022-12-23
-
-const solution = angle => {
-    return angle < 90 ? 1 : angle === 90 ? 2 : angle === 180 ? 4 : 3
-}
-
-const another_solution = angle => [0, 90, 91, 180].filter(a => angle >= a).length
